303cba48316305051065c0e8b29a4a5b176eb4e4
Drew Powers <drew@pow.rs>
2019-10-08 21:51:04 -0600

    Experimental CSS Modules

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 import { h, render } from "/web_modules/preact.js";
 import App from "./components/App.js";
+import css from "/static/app.min.css";
+document.styleSheets.push(css);
 var appMount = document.querySelector("#app");
 if (appMount)
     render(h(App, null), appMount);
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { h, render } from "/web_modules/preact.js";
 import App from "./components/App.js";
+import css from "/static/app.min.css";
+
+(document.styleSheets as any).push(css);
 
 const appMount = document.querySelector("#app");
 if (appMount) render(<App />, appMount);
